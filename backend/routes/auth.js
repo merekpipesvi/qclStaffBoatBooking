e05708c78977c5f76bd61e58bf57898d9dd068e6
Merek Pipes <pipesj@union.edu>
2024-05-13 03:19:27 -0700

    Immediately deleting it

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -35,7 +35,6 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/me', validateToken, async (req, res) => {
-    console.log('hit')
     const currentUser = await getUserById(req.userId);
     res.json(currentUser);
 });
