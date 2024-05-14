854723d7023374053af183a767e8b7a40c6e2d14
Merek Pipes <pipesj@union.edu>
2024-05-14 02:39:09 -0700

    Making logo clickable

--- frontend/components/Logo/Logo.tsx
+++ frontend/components/Logo/Logo.tsx
@@ -1,3 +1,13 @@
 import { Image, ImageProps } from '@mantine/core';
+import { useRouter } from 'next/router';
 
-export const Logo = (props : Omit<ImageProps, 'src'>) => (<Image src="https://www.queencharlottelodge.com/wp-content/uploads/2016/09/qcl-haida-gwaii.png" {...props} />);
+export const Logo = (props : Omit<ImageProps, 'src'>) => {
+    const router = useRouter();
+    return(
+        <Image 
+            src="https://www.queencharlottelodge.com/wp-content/uploads/2016/09/qcl-haida-gwaii.png" 
+            onClick={() => router.push('/booking')} 
+            {...props} 
+        />
+    );
+};
