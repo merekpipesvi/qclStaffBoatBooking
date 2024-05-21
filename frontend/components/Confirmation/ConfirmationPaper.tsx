7b404259798b52d3858270ead960ee940d98848e
Merek Pipes <pipesj@union.edu>
2024-05-20 21:21:31 -0700

    It was the DateTime on MYSQLs server end

--- frontend/components/Confirmation/ConfirmationPaper.tsx
+++ frontend/components/Confirmation/ConfirmationPaper.tsx
@@ -21,8 +21,6 @@ const NoConfirmationsAvailable = ({ isAfterCutOff, hasNoBookings }:
 
 export const ConfirmationPaper = () => {
     const { isAfterConfirmCutOff, isAfterSignUpCutOff} = useCutOffTimes();
-    console.log({date: new Date(), isAfterConfirmCutOff, isAfterSignUpCutOff})
-    console.log({eval: (!isAfterConfirmCutOff && !isAfterSignUpCutOff)})
     const isOutOfConfirmationWindow = isAfterConfirmCutOff || (!isAfterConfirmCutOff && !isAfterSignUpCutOff);
     const { data, refetch: refetchBookings } = useGetMyBookingsNeedingConfirmationQuery(undefined, {
         skip: isOutOfConfirmationWindow,
